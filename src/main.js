9e39b9a283abcc7136c5cf4ed7f3d851041b4f84
Emily Kinder <89366821+EmKinder@users.noreply.github.com>
2023-11-30 14:45:00 +1100

    handy added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 "use strict";
-import * as THREE from 'https://unpkg.com/three/build/three.module.js'
+import * as THREE from 'https://unpkg.com/three@latest'
 import { VRButton } from 'https://unpkg.com/three@latest/examples/jsm/webxr/VRButton.js';
-import {XRButton} from 'https://unpkg.com/three@0.158.0/examples/jsm/webxr/XRButton.js';
+import {Handy} from 'handy.js-master/src/Handy.js';
+import {XRHandModelFactory} from 'https://unpkg.com/three@latest/examples/jsm/webxr/XRHandModelFactory.js';
+
 //var http = require('http');
-
-
 var width = window.innerWidth;
 var height = window.innerHeight;
 var rotationVal = 0.05;
@@ -13,23 +13,90 @@ var prevMouseX;
 var mouseY;
 var prevMouseY;
 var cubeRotate = false;
+
+//THREE SETUP
+
 var renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('app')
+    canvas: document.getElementById('app'), antialias: true
 });
 renderer.setSize(width, height);
-var mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100); //FOV, aspect, near plane, far plane
+renderer.setPixelRatio(window.devicePixelRatio);
+var mainCamera = new THREE.PerspectiveCamera(75, width / height, 0.01, 1000); //FOV, aspect, near plane, far plane
 var scene = new THREE.Scene();
+scene.add(mainCamera)
+
+world = new THREE.Group()
+scene.add( world )
+
+renderer.xr.enabled = true;
+document.body.appendChild(VRButton.createButton(renderer));
+document.body.appendChild(renderer.domElement);
+
+var light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(0, 4, 2);
+scene.add(light);
+
+window.addEventListener( 'resize', function(){
+	
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    controls.update()
+
+}, false )
+
+
+
+
+window.THREE = THREE
+window.Handy = Handy
+
+function setupHands(){
+    const handModelFactory = new XRHandModelFactory(), 
+    cycleHandModel = function(event){
+        const hand = event.hand
+    
+    hand.models.forEach(function(model){
+        model.visible = false
+    })
+    hand.modelIndex = (hand.modelIndex + 1) % hand.models.length
+    hand.models[hand.modelIndex].visible = true
+    }
+    const [hand0, hand1]= [ {}, {}].map(function(hand, i){
+        hand = renderer.xr.getHand(i)
+        scene.add(hand)
+        hand.models = [
+            handModelFactory.createHandModel(hand, 'boxes'),
+            handModelFactory.createHandModel(hand, 'spheres'),
+            handModelFactory.createHandModel(hand, 'mesh')
+        ]
+        hand.modelIndex = 0
+
+        Handy.makeHandy(hand)
+
+        hand.displayFrameAnchor = new THREE.Object3D()
+        hand.add(hand.displayFrameAnchor)
+
+        hand.addEventListener('connected', function(event){
+            hand.handedness = event.data.handedness
+            hand.models.forEach(function(model){
+                hand.add(model)
+                model.visible = false
+            })
+            hand.models[hand.modelIndex].visible = true
+        })
+    })
+    return hand
+}
+
 var geometry = new THREE.BoxGeometry;
 var material = new THREE.MeshPhongMaterial({ color: 0xFFAD00 });
 var cube = new THREE.Mesh(geometry, material);
 cube.position.z = -5;
 cube.position.y = 1;
 scene.add(cube);
-var light = new THREE.DirectionalLight(0xFFFFFF, 1);
-light.position.set(0, 4, 2);
-scene.add(light);
-document.body.appendChild(XRButton.createButton(renderer));
-renderer.xr.enabled = true;
+
+
 function updateLoop() {
     /* Move rotation controls to a different class */
     renderer.render(scene, mainCamera);
@@ -70,3 +137,11 @@ document.addEventListener('mousedown', function () {
 document.addEventListener('mouseup', function (event) {
     cubeRotate = false;
 });
+
+window.addEventListener( 'DOMContentLoaded', function(){
+
+	//setupThree()
+	//Bolt.setup( scene )
+	setupHands()
+	//setupContent()
+})
