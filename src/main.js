27ebdf0a004e9d42611e0cd152d51b4e962b614d
Emily Kinder <89366821+EmKinder@users.noreply.github.com>
2023-11-30 16:41:10 +1100

    more of the same

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 "use strict";
-import * as THREE from '../node_modules/three/build/three.module.js'; // Adjust the path
-import { VRButton } from '../node_modules/three/examples/jsm/webxr/VRButton.js'; // Adjust the path
+import * as THREE from 'three'; // Adjust the path
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'; // Adjust the path
 import { Handy } from '../node_modules/three/examples/jsm/webxr/Handy.js'; // Adjust the path
-import { XRHandModelFactory } from '../node_modules/three/examples/jsm/webxr/XRHandModelFactory.js'; // Adjust the path
+import { XRHandModelFactory } from 'three/examples/jsm/webxr/XRHandModelFactory.js'; // Adjust the path
 
 
 //var http = require('http');
